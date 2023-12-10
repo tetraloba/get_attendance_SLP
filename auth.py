532d7226754b1a3c393a6e10145d9c0922b6666b
tetraloba <103760922+tetraloba@users.noreply.github.com>
2023-12-10 22:45:03 +0900

    fix_typo

--- auth.py
+++ auth.py
@@ -1,2 +1,2 @@
 discord_info = {'token':'PASTE_YOUR_TOKEN_HERE', 'channel_id':0000000000000000000}
-ssh_info = {'host':'PASTE_HOSTNAME_OR_IP-ADDRESS', 'port':00000, 'user':'PAST_USERNAME', 'password':'PASTE_PASSWORD'}
+ssh_info = {'host':'PASTE_HOSTNAME_OR_IP-ADDRESS', 'port':00000, 'user':'PASTE_USERNAME', 'password':'PASTE_PASSWORD'}

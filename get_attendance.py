0d57d799269ffbd7a9d7f8ef67fd5ad8adf28098
tetraloba <four.leaves.siva7342@gmail.com>
2023-12-23 13:58:03 +0900

    data.jsonの更新日時を監視 -> ファイルサイズを監視 #1

--- get_attendance.py
+++ get_attendance.py
@@ -24,7 +24,7 @@ client = discord.Client(intents=discord.Intents.default())
 channel = None # 通知を行うチャンネル
 
 file_path = 'attendance_system/data.json' # 監視対象のファイルパス
-get_mdate = f"date -r {file_path} +%d%H%M%S" # ファイルの更新日時を取得するShell Script
+get_fsize = f"du -b {file_path} | cut -f 1" # ファイルのサイズ(B)を取得するShell Script
 
 # Discord BOT の準備ができたら実行される
 @client.event
@@ -36,15 +36,15 @@ async def on_ready():
 # 2秒毎に自動実行される
 @tasks.loop(seconds=2)
 async def get_attendance():
-    global p # 前回の更新日時
-    c = ssh.exec_command(get_mdate)[1].readline() # 現在の更新日時
-    if c != p:
+    global p # 前回のファイルサイズ
+    c = ssh.exec_command(get_fsize)[1].readline() # 現在のファイルサイズ
+    if p < c: # ファイルサイズが増加した場合
         # 学籍番号と日時の情報を discord で発言
         await channel.send(ssh.exec_command(f'cat {file_path} | tail -n 4 | tr "\n" " " | cut -b 19-27,50-70')[1].readline())
-        p = c
+    p = c
 
 try:
-    p = ssh.exec_command(get_mdate)[1].readline() # 前回の更新日時
+    p = ssh.exec_command(get_fsize)[1].readline() # 前回のファイルサイズ
 
     client.run(TOKEN) # Discord BOT を起動
 
